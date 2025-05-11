48a846c7baf89f594dcca09ce2401c51c4e16431
vanhieuhp <vanhieuit10@mail.com>
2025-05-11 10:46:12 +0700

    feat: finish tic-tac-toe game

--- tic_tac_toe_game/src/App.jsx
+++ tic_tac_toe_game/src/App.jsx
@@ -1,10 +1,101 @@
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import {useState} from "react";
+import Log from "./components/Log.jsx";
+import {WINNING_COMBINATIONS} from "./winning-combination.js";
+import GameOver from "./components/GameOver.jsx";
+
+const PLAYERS = {
+    "X": "player-1",
+    "O": "player-2"
+}
+
+const INITIAL_GAME_BOARD = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+]
+
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = "X";
+    if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+        currentPlayer = "O";
+    }
+    return currentPlayer;
+}
+
+function deriveWinner(gameBoard, players) {
+    let winner;
+
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+        const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+        const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+        if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+            winner = players[firstSquareSymbol];
+        }
+    }
+
+    return winner;
+}
+
+function deriveGameBoard() {
+    let gameBoard = [...INITIAL_GAME_BOARD.map(row => [...row])];
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+        gameBoard[row][col] = player;
+    }
+    return gameBoard;
+}
 
 function App() {
-  
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    const [gameTurns, setGameTurns] = useState([]);
+    const [players, setPlayers] = useState(PLAYERS)
+
+    const activePlayer = deriveActivePlayer(gameTurns);
+    const gameBoard = deriveGameBoard();
+    const winner = deriveWinner(gameBoard, players);
+    const hasDraw = gameTurns.length === 9 && !winner;
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameTurns(prevTurns => {
+            const currentPlayer = deriveActivePlayer(gameTurns);
+            const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns];
+            return updatedTurns;
+        });
+    }
+
+    function resetGameBoard() {
+        setGameTurns([]);
+    }
+
+    function handlePlayerNameChange(symbol, newName) {
+        setPlayers(prevPlayers => {
+            return {...prevPlayers, [symbol]: newName}
+        })
+    }
+
+    return (<main>
+        <div id="game-container">
+            <ol id="players" className="highlight-player">
+                <Player initialName={PLAYERS.X}
+                        symbol="X"
+                        isActive={activePlayer === "X"}
+                        onChangeName={handlePlayerNameChange}/>
+                <Player initialName={PLAYERS.O}
+                        symbol="O"
+                        isActive={activePlayer === "O"}
+                        onChangeName={handlePlayerNameChange}/>
+            </ol>
+            {(winner || hasDraw) && <GameOver winner={winner} onReset={resetGameBoard}/>}
+            <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
+        </div>
+        <Log turns={gameTurns}/>
+    </main>)
 }
 
 export default App
---
+++ tic_tac_toe_game/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import {useState} from "react";
+
+export default function Player({initialName, symbol, isActive, onChangeName}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEdit() {
+        setIsEditing((editing) => !editing)
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+    }
+
+    return (
+        <li className={isActive ? "active" : undefined}>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+    )
+}
